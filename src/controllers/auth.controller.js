c8404cbe7864872a7b0f8834b95d120dd8db566b
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-27 11:53:24 +0700

    feat: create controller for login and register

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,13 +1,77 @@
+import { validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+import process from "node:process";
+import { verifyPassword } from "../lib/hashPasswordArgon2.js";
+import {
+  addUser,
+  checkUserEmail,
+  getUserByEmail,
+} from "../models/users.model.js";
+
 /**
  * POST /auth/login
  * @summary Login user
  * @tags authentication
  * @param  {string} email.form.required - Email of user - application/x-www-form-urlencoded
  * @param  {string} password.form.required - Password of user - application/x-www-form-urlencoded
- * @return {object} 200 - login success
- * @return {object} 401 - wrong email or password
+ * @return {object} 200 - Login successfull
+ * @return {object} 401 - Incorrect email or password
  */
-export async function login(req, res) {}
+export async function login(req, res) {
+  try {
+    const result = validationResult(req);
+
+    if (!result.isEmpty()) {
+      res.status(400).json({
+        success: false,
+        message: "Please provide valid email and password",
+        results: result.array(),
+      });
+      return;
+    }
+
+    const { email, password } = req.body;
+
+    const user = await getUserByEmail(email);
+
+    if (!user) {
+      res.status(400).json({
+        success: false,
+        message: "Incorrect email or password",
+      });
+      return;
+    }
+
+    const isValidPassword = await verifyPassword(user.password, password);
+
+    if (!isValidPassword) {
+      res.status(401).json({
+        success: false,
+        message: "Incorrect email or password",
+      });
+      return;
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.APP_SECRET, {
+      expiresIn: 15 * 60 * 1000,
+    });
+
+    res.json({
+      success: true,
+      message: "Login successfull!",
+      results: {
+        token,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Login failed",
+      error: err.message,
+    });
+    return;
+  }
+}
 
 /**
  * POST /auth/register
@@ -16,9 +80,52 @@ export async function login(req, res) {}
  * @param  {string} fullName.form.required - fullname of user - application/x-www-form-urlencoded
  * @param  {string} email.form.required - Email of user - application/x-www-form-urlencoded
  * @param  {string} password.form.required - Password of user - application/x-www-form-urlencoded
+ * @param  {string} role.form - Role of user - application/x-www-form-urlencoded
  * @return {object} 200 - login success
  */
-export async function register(req, res) {}
+export async function register(req, res) {
+  try {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      res.status(400).json({
+        success: false,
+        message: "Please provide valid registration information",
+        results: result.array(),
+      });
+      return;
+    }
+
+    let data = req.body;
+    if (data.role) {
+      data = { ...data, role: "customer" };
+    }
+
+    const exists = await checkUserEmail(data.email);
+
+    if (exists) {
+      res.status(409).json({
+        success: false,
+        message: "Email is already registered",
+      });
+      return;
+    }
+
+    const user = await addUser(data);
+
+    res.json({
+      success: true,
+      message: "User registered successfully",
+      results: user,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Register failed",
+      error: err.message,
+    });
+    return;
+  }
+}
 
 /**
  * POST /auth/logout
@@ -26,4 +133,4 @@ export async function register(req, res) {}
  * @tags authentication
  * @return {object} 200 - logout success
  */
-export async function logout(req, res) {}
+// export async function logout(req, res) {}
