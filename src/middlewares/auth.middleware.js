cbe27637ea7c5d2f598a79b4c87694b54225c308
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-27 11:18:25 +0700

    feat: create auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,7 +1,34 @@
+import jwt from "jsonwebtoken";
+import process from "process";
+
 /**
  *
  * @param {import("express").Request} req
  * @param {import("express").Response} res
  * @param {import("express").NextFunction} next
  */
-export function authMiddleware(req, res, next) {}
+export function authMiddleware(req, res, next) {
+  const bearer = req.headers?.authorization ?? "";
+  const prefix = "Bearer ";
+  if (!bearer.startsWith(prefix)) {
+    res.status(401).json({
+      success: false,
+      message: "Authorization header required or invalid format",
+    });
+    return;
+  }
+
+  const token = bearer.substring(prefix.length);
+
+  try {
+    const payload = jwt.verify(token, process.env.APP_SECRET);
+    req.jwtPaload = payload;
+    next();
+  } catch (err) {
+    res.status(401).json({
+      success: false,
+      message: "Failed to verify token",
+      result: err.message,
+    });
+  }
+}
