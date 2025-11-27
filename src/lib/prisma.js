4557cade51e741a44b505e9f00d42a7bab10fff6
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-27 11:51:45 +0700

    fix: import on prisma client

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,6 +1,6 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import process from "node:process";
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "../../generated/prisma/client.ts";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
