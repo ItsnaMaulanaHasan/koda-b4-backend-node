17197dc9b42b2d55ad69ca79f9a08447ce882059
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-27 11:52:46 +0700

    feat: create function to addUser and checkUserByEmail

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,3 +1,4 @@
+import { hashPassword } from "../lib/hashPasswordArgon2.js";
 import { prisma } from "../lib/prisma.js";
 
 export async function getUserByEmail(email) {
@@ -11,6 +12,40 @@ export async function getUserByEmail(email) {
     return result;
   } catch (err) {
     console.log("Error while get data user by email: ", err);
+    throw err;
+  }
+}
+
+export async function addUser(data) {
+  try {
+    const hashed = await hashPassword(data.password);
+    const result = await prisma.user.create({
+      data: {
+        fullName: data.fullName,
+        email: data.email,
+        password: hashed,
+      },
+    });
+    return result;
+  } catch (err) {
+    console.log("Error while register: ", err);
+    throw err;
+  }
+}
+
+export async function checkUserEmail(email) {
+  try {
+    const result = await prisma.user.findFirst({
+      where: { email: email },
+    });
+
+    if (result) {
+      return true;
+    }
+
+    return false;
+  } catch (err) {
+    console.error("Error get user by email:", err);
     throw err;
   }
 }
