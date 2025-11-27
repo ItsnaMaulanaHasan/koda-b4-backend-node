128c7b6da4c965d8b5a278454bc1f23bf64fd6c8
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-27 11:32:02 +0700

    feat: create funtion to getUserByEmail for register validation

---
+++ src/models/users.model.js
@@ -0,0 +1,16 @@
+import { prisma } from "../lib/prisma.js";
+
+export async function getUserByEmail(email) {
+  try {
+    const result = await prisma.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    return result;
+  } catch (err) {
+    console.log("Error while get data user by email: ", err);
+    throw err;
+  }
+}
