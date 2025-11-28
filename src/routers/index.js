9f8669c58a11833a165627dc7ffb0ed9da7f8bae
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-28 10:57:33 +0700

    feat: create route for serving static file

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,4 +1,6 @@
-import { Router } from "express";
+import express, { Router } from "express";
+import path from "node:path";
+import process from "node:process";
 import authRouter from "./auth.router.js";
 import usersRouter from "./users.admin.route.js";
 
@@ -6,5 +8,6 @@ const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/admin/users", usersRouter);
+router.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 export default router;
