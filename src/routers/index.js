83be05dee4c00c5311857d7e5a7c2542073d404b
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-28 15:00:58 +0700

    feat: create middleware authorization

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,13 +1,14 @@
 import express, { Router } from "express";
 import path from "node:path";
 import process from "node:process";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 import authRouter from "./auth.router.js";
 import usersRouter from "./users.admin.route.js";
 
 const router = Router();
 
 router.use("/auth", authRouter);
-router.use("/admin/users", usersRouter);
+router.use("/admin/users", authMiddleware, usersRouter);
 router.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 export default router;
