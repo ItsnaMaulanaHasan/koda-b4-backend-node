b4d718bbc2de1d983a27c2477e5d618edf8e15a2
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-29 16:29:34 +0700

    fix: configuration prisma for deploy vercel

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,12 +1,17 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import pkg from "@prisma/client";
+import { attachDatabasePool } from "@vercel/functions";
 import process from "node:process";
+import { Pool } from "pg";
 
 const { PrismaClient } = pkg;
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaPg({ connectionString });
+const pool = new Pool({ connectionString });
+attachDatabasePool(pool);
+
+const adapter = new PrismaPg({ pool });
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
