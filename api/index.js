22ba6d45c75c9f0a9d8d9bf110bf6289e049bbe5
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-29 17:58:53 +0700

    refactor: restore initial configuration

--- api/index.js
+++ api/index.js
@@ -6,11 +6,7 @@ import router from "../src/routers/index.js";
 
 const app = express();
 
-try {
-  initDocs(app);
-} catch (error) {
-  console.error("Error initializing docs:", error);
-}
+initDocs(app);
 
 app.use(urlencoded({ extended: true }));
 app.use(json());
@@ -24,14 +20,4 @@ app.get("/", (req, res) => {
 
 app.use("/", router);
 
-app.use((err, req, res) => {
-  console.error(err.stack);
-  res.status(500).json({
-    success: false,
-    message: "Internal server error",
-  });
-});
-
-export default serverless(app, {
-  binary: ["image/*", "application/pdf"],
-});
+export default serverless(app);
