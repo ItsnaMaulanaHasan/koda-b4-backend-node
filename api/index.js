c377277f4703edec25ee7f83cda3ca4b1f5b152c
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-29 15:17:02 +0700

    feat: create vercel.json and folder api to set up deploy to vercel

---
+++ api/index.js
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import express, { json, urlencoded } from "express";
+import serverless from "serverless-http";
+import initDocs from "./src/lib/docs.js";
+import router from "./src/routers/index.js";
+
+const app = express();
+
+initDocs(app);
+
+app.use(urlencoded({ extended: true }));
+
+app.use(json());
+
+app.use("/", router);
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Backend is running well",
+  });
+});
+
+export default serverless(app);
