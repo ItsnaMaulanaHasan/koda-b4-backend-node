9482a3d1f1eda867bd7d23264fbeacf95a63bfb8
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-12-01 01:55:25 +0700

    fix: api/index.js

--- api/index.js
+++ api/index.js
@@ -1,32 +1,13 @@
 import "dotenv/config";
 import express from "express";
 import serverless from "serverless-http";
-import initDocs from "../src/lib/docs.js";
-import { initRedis } from "../src/lib/redis.js";
 import { corsMiddleware } from "../src/middlewares/cors.middleware.js";
 import router from "../src/routers/index.js";
 
 const app = express();
-
-let initialized = false;
-
-async function initOnce() {
-  if (initialized) return;
-  await initRedis();
-  initDocs(app);
-  initialized = true;
-}
-
-app.use(async (req, res, next) => {
-  await initOnce();
-  next();
-});
-
 app.use(corsMiddleware());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use("/", router);
 
 app.get("/", (req, res) => {
   res.json({
@@ -34,5 +15,13 @@ app.get("/", (req, res) => {
     message: "Backend is running well from serverless",
   });
 });
+
+app.use("/api", router);
+
+import initDocs from "../src/lib/docs.js";
+import { initRedis } from "../src/lib/redis.js";
+
+await initRedis().catch((err) => console.error("Redis init failed:", err));
+initDocs(app);
 
 export default serverless(app);
