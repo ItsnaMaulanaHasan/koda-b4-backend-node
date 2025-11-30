1a732484b24e9abdabf123f5b231ff6b1642408e
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-12-01 01:10:49 +0700

    fix: index api for entry point vercel

--- api/index.js
+++ api/index.js
@@ -2,14 +2,21 @@ import "dotenv/config";
 import express, { json, urlencoded } from "express";
 import serverless from "serverless-http";
 import initDocs from "../src/lib/docs.js";
+import { initRedis } from "../src/lib/redis.js";
+import { corsMiddleware } from "../src/middlewares/cors.middleware.js";
 import router from "../src/routers/index.js";
 
 const app = express();
 
+await initRedis();
+
 initDocs(app);
 
+app.use(corsMiddleware());
 app.use(urlencoded({ extended: true }));
 app.use(json());
+
+app.use("/", router);
 
 app.get("/", (req, res) => {
   res.json({
